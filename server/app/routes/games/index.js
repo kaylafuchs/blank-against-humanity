9e530371917f29a3150b2dea02c8548092d741b3
Dan Mandel <dan19926@gmail.com>
2016-09-30 17:07:17 -0400

    added server folder from fsg

---
+++ server/app/routes/games/index.js
@@ -0,0 +1,17 @@
+'use strict';
+var router = require('express').Router(); // eslint-disable-line new-cap
+module.exports = router;
+
+// var ensureAuthenticated = function (req, res, next) {
+//     if (req.isAuthenticated()) {
+//         next();
+//     } else {
+//         res.status(401).end();
+//     }
+// };
+
+router.get('/', (req, res) => {
+
+    
+
+});
---
+++ server/db/index.js
@@ -0,0 +1,42 @@
+'use strict';
+const db = require('./_db');
+module.exports = db;
+
+// eslint-disable-next-line no-unused-consts
+const Card = require('./models/card')
+const Game = require('./models/game');
+const Round = require('./models/round');
+const Team = require('./models/team');
+const User = require('./models/user');
+
+
+User.belongsToMany(Game, {through: 'player_games'}); 
+User.belongsToMany(Round, {through: 'user_round'}); //dont need this because rounds belong to a game? 
+User.belongsTo(Team, {as: 'userTeam'}); //User.belongsTo(Team)
+User.hasMany(Card, {as: 'created_cards'});
+User.belongsToMany(Card, {through: 'tagged_cards'}); //can tag many people in a card
+
+//Channel.belongsTo(team)
+
+Team.hasMany(User); 
+Team.hasMany(Game);
+Team.hasMany(Card);
+
+Card.belongsTo(Team); //change to channel
+Card.belongsToMany(Game, {through: 'game_cards'});
+Card.belongsTo(User, {as: 'author'})
+Card.belongsTo(User, {as: 'taggedUser'})
+
+
+Round.hasOne(Card, {as: 'blackCard'}); 
+Round.hasMany(Card, {as: 'cardsPlayed'});
+
+Round.belongsTo(Game); 
+Round.belongsToMany(User, {through: 'player'});
+Round.belongsToMany(User, {through: 'judge'}); //model was_judge?
+
+Game.hasMany(Round);
+Game.belongsToMany(User, {through: 'player'});
+Game.belongsTo(Team);
+Game.hasMany(Round);
+
