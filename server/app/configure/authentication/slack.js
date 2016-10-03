0fec31dc5673668f766a39e9cb18887f05b5aec2
Nithya Mathews <nithyarmathews@gmail.com>
2016-10-03 17:12:45 -0400

    added slack oatch

---
+++ server/app/configure/authentication/slack.js
@@ -0,0 +1,46 @@
+'use strict';
+var passport = require('passport');
+var SlackStrategy = require('passport-slack').Strategy;
+
+module.exports = function (app, db) {
+
+    var User = db.model('user');
+
+    var slackConfig = app.getValue('env').SLACK; //Is this correct?
+
+    var slackCredentials = {
+        clientID: slackConfig.clientID,
+        clientSecret:slackConfig.clientSecret,
+        callbackURL: slackConfig.callbackURL
+        scope: 'channels:read chat:write:bot team:read',
+    };
+
+    var verifyCallback = function (accessToken, refreshToken, profile, done) {
+
+        User.findOrCreate({
+                where: {
+                    SlackId: profile.id
+                }
+            })
+            .then(function (user) {
+                return done(null, user);
+            })
+            .catch(function (err) {
+                console.error('Error creating user from Slack authentication', err);
+                done(err);
+            })
+
+    };
+
+    passport.use(new SlackStrategy(slackCredentials, verifyCallback));
+
+    app.get('/auth/slack', passport.authorize('slack'));
+
+    app.get('/auth/slack/callback', 
+        passport.authorize('slack', { failureRedirect: '/login' }),
+        function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
+
+};
