c65b3ab275821d5465c656467229844e8d0cd91c
kaylafuchs <kfuchs24@gmail.com>
2016-10-04 11:02:57 -0400

    added bottom nav

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -5,6 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 window.app = angular.module('BlankAgainstHumanity', ['ionic', 'ui.router'])
 
+console.log("APP", app)
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
