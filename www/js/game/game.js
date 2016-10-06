3a5496063abb20ed1544ae5d0b21dc7b4b1f42f8
Dan Mandel <dan19926@gmail.com>
2016-10-06 16:36:21 -0400

    working on newgame state

--- www/js/game/game.js
+++ www/js/game/game.js
@@ -6,46 +6,50 @@ app.config(($stateProvider) => {
             templateUrl: 'js/game/game.html',
             //abstract: true,
             controller: 'GameCtrl',
-            resolve: { //this could still get the team games even without stateparams by using /session
-                teamGames: (GameFactory, $stateParams) => GameFactory.getGamesByTeamId($stateParams.teamId)
-            }
+            // resolve: { //this could still get the team games even without stateparams by using /session
+            //     teamGames: (GameFactory, $stateParams) => GameFactory.getGamesByTeamId($stateParams.teamId)
+            // }
+
 
 
         })
-        // .state('game.gameview', {
-        //     url: '', // '' means this state becomes active when the parent state is navigated tp
-        //     templateUrl: 'js/game/team-games.html',
-        //     //controller: ''
-        //     resolve: {
-        //         teamGames: (GameFactory, $stateParams) => GameFactory.getGamesByTeamId($stateParams.teamId) 
-        //     }
-        // })
-        // // maybe make this the join game screen
-        // .state('game.allTeamGames', {
-        //     url: '/game/team',
-        //     templateUrl: 'js/game/team-games.html',
-        //     //controller: ''
-        //     resolve: {
-        //         teamGames: (GameFactory, $stateParams) => GameFactory.getGamesByTeamId($stateParams.teamId) 
-        //     }
-        // })
-        //.state('game.pickDecks')
+        .state('game.newGame', {
+            url: '/game/new-game', // '' means this state becomes active when the parent state is navigated tp
+            templateUrl: 'js/game/new-game.html'
+                //controller: ''
+                // resolve: {
+                //     teamGames: (GameFactory, $stateParams) => GameFactory.getGamesByTeamId($stateParams.teamId)
+                // }
+        })
+
+
+    // maybe make this the join game screen
+    // .state('game.allTeamGames', {
+    //     url: '/game/team',
+    //     templateUrl: 'js/game/team-games.html',
+    //     //controller: ''
+    //     resolve: {
+    //         teamGames: (GameFactory, $stateParams) => GameFactory.getGamesByTeamId($stateParams.teamId)
+    //     }
+    // })
+    // .state('game.pickDecks')
+
 
 
 })
 
-app.controller('GameCtrl', ($scope, GameFactory, teamGames) => {
+app.controller('GameCtrl', ($scope, GameFactory) => {
     console.log('running gamecrl')
     $scope.startNewGame = GameFactory.startNewGame;
     $scope.addDecksToGame = GameFactory.addDecks
     $scope.$on('changedGame', (event, data) => {
-        console.log('received event')
-        console.log('data obj:', data)
-        $scope.game = data;
-        $scope.$digest()
+            console.log('received event')
+            console.log('data obj:', data)
+            $scope.game = data;
+            $scope.$digest()
 
-    })
-    $scope.games = teamGames;
+        })
+        //$scope.games = teamGames;
 
     //console.log('teamgames ', teamGames)
 })
