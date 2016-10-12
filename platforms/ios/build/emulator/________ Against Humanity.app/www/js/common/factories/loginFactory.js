55c4b42ec1d79500ad81938a6a7da42914e9732a
kaylafuchs <kfuchs24@gmail.com>
2016-10-11 23:15:04 -0400

    swipe works, updated active game state, added app icon

--- platforms/ios/build/emulator/________ Against Humanity.app/www/js/common/factories/loginFactory.js
+++ platforms/ios/build/emulator/________ Against Humanity.app/www/js/common/factories/loginFactory.js
@@ -1,7 +1,7 @@
 app.factory('LoginFactory', function($http){
 	return {
 		getSlackCreds: function(){
-			return $http.get('http://localhost:1337/api/slack')	
+			return $http.get('http://192.168.0.2:1337/api/slack')	
 				.then(res => {
 					return res.data
 				})
