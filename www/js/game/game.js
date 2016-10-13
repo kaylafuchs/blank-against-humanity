01dbbdbb3448484bf4514510695022ffccf25467
kaylafuchs <kfuchs24@gmail.com>
2016-10-13 13:02:57 -0400

    updates for demo

--- www/js/game/game.js
+++ www/js/game/game.js
@@ -24,21 +24,32 @@ app.controller("ActiveGameCtrl", ($scope, GameFactory, ActiveGameFactory, game, 
 
     const gameId = $stateParams.gameId;
     const playerId = $localStorage.user.id;
+    console.log("player id", playerId)
     const teamId = $localStorage.team.id
     $scope.game = game;
     $scope.gameName = $scope.game.settings.name;
-    console.log("active state game", JSON.stringify($scope.game));
-
+    console.log("active state game", $scope.game);
+    $scope.judge = $scope.game.players[$scope.game.currentJudge]
+    console.log("the judge is", $scope.judge)
+    $scope.blackCard = $scope.game.currentBlackCard;
+    $scope.blackCardText = $scope.blackCard[Object.keys($scope.blackCard)[0]].text
+    console.log("the black card is", $scope.blackCardText)
     //this should be uncommented in final versions
-    //$scope.whiteCards = $scope.game.players[playerId].hand;
+    $scope.whiteCards = $scope.game.pile.whitecards;
+    var slicer = Math.floor(Math.random() * $scope.whiteCards.length - 7)
+    // $scope.playerHand = $scope.whiteCards.slice(slicer, slicer + 8)
+    $scope.playerHand = $scope.game.players[playerId].hand;
+    console.log('players hand', $scope.playerHand)
 
     //temporary implementation for design purposes.
     // $scope.game.whiteCards = $scope.game.pile.whitecards
     $scope.showCards = false;
 
+    //$scope.playerHand = $scope.game.players[playerId].hand;
+    //(console.log("player hand", $scope.hand))
+
     $scope.playerCount = Object.keys($scope.game.players).length;
     
-    console.log('WHITECARDS', $scope.whiteCards);
 
     $scope.onSwipeDown = () => {
         $scope.showCards = true;
@@ -53,11 +64,20 @@ app.controller("ActiveGameCtrl", ($scope, GameFactory, ActiveGameFactory, game, 
         //unique id
     }
 
+    //still need to be able to get player's hand from deck and set it on scope so we can render it
+
+    $scope.onDoubleTap = (key) => {
+        console.log("double tapped")
+        $scope.played = true;
+        //call submit card function here.
+    }
+
     ActiveGameFactory.refillMyHand(gameId, playerId, teamId);
 
     $scope.$on('changedGame', (event,snapshot) =>{
         $scope.game = snapshot;
     })
+    //need to listen for judge choice, refill hand after round
     
 })
 
